707ed89bbaba9f3b9b342c20491ece3c795f3bba
Reza <mohammadreza.chegini01@gmail.com>
2023-04-21 17:38:16 -0700

    add shoping cart

--- src/components/CountryDetails.vue
+++ src/components/CountryDetails.vue
@@ -6,25 +6,37 @@
              <li>Population: {{ countryDetail.population }}</li>
         </ul>
         <button @click="closeDeatil" >Close </button>
+        <button @click="handleAddButton">Add to cart</button>
+        <shopping-list v-if="!showComponent" :chosenCountry="chosenCountry" ></shopping-list>
     </div>
 </template>
 
 <script>
+    import ShoppingList from './ShoppingList.vue';
 
     export default{
         name: "CountryDetails",
+        components:{
+            ShoppingList
+        },
         props:{
             countryDetail: Object
         },
     
         data(){
             return{
-
+                chosenCountry: [],
+                showComponent:true
             }
         },
         methods:{
             closeDeatil(){
                 this.$emit('close-details')
+            },
+            handleAddButton(event){
+                event.preventDefault();
+                this.chosenCountry.push(this.countryDetail['name']);
+                this.showComponent = false;
             }
         },
 
---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h1>Country Population</h1>
+        <ul>
+            <li>Name: {{ chosenCountry.name }}</li>
+        </ul>
+
+
+        <ul v-if="chosenCountry.length">
+            <li v-for="(ct, index) in chosenCountry" :key="index">
+                {{ ct }}
+            </li>
+        </ul>
+        <!-- <button @click="closeDeatil" >Close </button> -->
+    </div>
+</template>
+
+<script>
+
+    export default{
+        name: "shoppingList",
+        props:{
+            chosenCountry: Array
+        },
+    
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+
+        },
+
+        mounted(){
+        },
+
+        watch:{
+            
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
